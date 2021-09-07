cd88b5a6a69f24481f35a917bdca316b6b27e813
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-07 12:36:39 +0530

    feat:insreted

---
+++ uc11.sql
@@ -0,0 +1,6 @@
+use addressBook
+
+INSERT INTO address(firstName, LastName, Address, City, State, Zip, PhoneNumber, Email, BookName, BookType) 
+VALUES
+('sowji','mani','#22 !st Cross','nrt','Andhrapradesh',523301,'8499875245','manikanta@gmail.com','Book2','Family'),
+('honey','manikanta','#22 !st Cross','nrt','Andhrapradesh',523301,'8499875245','manikanta@gmail.com','Book1','Friends');
