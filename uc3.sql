aafef0d40d12a912b013ebc28ceb149c8f565ced
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-07 11:54:47 +0530

    feat:insertValues

---
+++ uc3.sql
@@ -0,0 +1,5 @@
+--USE addressBook
+
+INSERT INTO address(firstName,lastName,address,City,State,Zip,phoneNumber,Email) VALUES
+('prakash','kandula','ballikurava main road','ballikurava','AndhraPradesh','523301','8499875245','abhinayprakash@gmail.com'),
+('mani','polysetti','amalapuram','rjy','AndhraPradesh','523301','8096713989','manikanta@gamil.com');
