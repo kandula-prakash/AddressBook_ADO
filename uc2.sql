6f8bf8ea0f2a2f68a5b3207b32bd5c9ef8417b96
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-07 11:28:27 +0530

    feat:addingValues

--- uc2.sql
+++ uc2.sql
@@ -1,4 +1,3 @@
---create database addressBook
 --USE addressBook
 
 create TABLE address(firstName VARCHAR(255)NOT NULL,lastName VARCHAR(255) NOT NULL,address VARCHAR(255) NOT NULL,City VARCHAR(100) NOT  NULL,State VARCHAR(100)NOT NULL,Zip VARCHAR(6)NOT NULL,phoneNumber VARCHAR(12) NOT NULL,Email VARCHAR(100)NOT NULL,);
